1fade5873bc4a2a3663d90bef58bb0f7b6071025
Serge Romanchuk <svromanchiuk@sbercloud.ru>
2024-03-23 17:52:07 +0300

    New.
    - Отправка данных в Kafka.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from db import get_source_data
+from _kafka import send_json_messages_to_queue
 
 
 if __name__ == '__main__':
     source_data = get_source_data()
+    topic = 'test'
+    send_json_messages_to_queue(topic, source_data)
 
     print()
