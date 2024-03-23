e89a049f82fcae54eddcd8836f520e88b40f42d5
Serge Romanchuk <svromanchiuk@sbercloud.ru>
2024-03-23 18:42:48 +0300

    New.
    - Добавил преобразование list и dict типов в строки.

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from os.path import join
 import postgres as pg
 import vertica as ve
 from file import get_script_content
-from misc import append_hash_to_data
+from misc import append_hash_to_data, dump_json_values
 
 PG_SCRIPT_FOLDER = join('scripts', 'pg')
 VE_SCRIPT_FOLDER = join('scripts', 've')
@@ -12,4 +12,4 @@ VE_SCRIPT_FOLDER = join('scripts', 've')
 
 def get_source_data() -> list:
     sql = get_script_content(join(PG_SCRIPT_FOLDER, 'get_data.sql'))
-    return append_hash_to_data(pg.execute_script(sql))
+    return dump_json_values(append_hash_to_data(pg.execute_script(sql)))
--- misc.py
+++ misc.py
@@ -1,4 +1,5 @@
 import hashlib
+import json
 
 
 def append_hash_to_data(data: list) -> list:
@@ -9,3 +10,18 @@ def append_hash_to_data(data: list) -> list:
 def make_hash_from_row_values(row: dict) -> str:
     """Хеш по значениям строки."""
     return hashlib.sha512(''.join(str(v) for v in row.values()).encode()).hexdigest()
+
+
+def dump_json_values(data: list) -> list:
+    """Преобразование данных типов list и dict в строки."""
+    results = []
+    for row in data[:]:
+        converted_row = {}
+        for k, v in row.items():
+            if isinstance(v, list) or isinstance(v, dict):
+                converted_row[k] = json.dumps(v, default=str, ensure_ascii=False)
+            else:
+                converted_row[k] = v
+        results.append(converted_row)
+
+    return results
